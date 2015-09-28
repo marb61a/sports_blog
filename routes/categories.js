07080e562cc9b4eac97998d6f959b881e8546bea
marb <marb61a@gmail.com>
2015-09-28 07:56:00 +0000

    updated categories routes

--- routes/categories.js
+++ routes/categories.js
@@ -43,4 +43,43 @@ router.post('/add', function(req, res){
     }
 });
 
+
+router.post('/edit/:id', function(req, res){
+	req.checkBody('title', 'Title is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('edit_category', {
+			errors: errors,
+			title: "Edit Category"
+		});
+	} else {
+		var category = new Category();
+		var query = {_id: [req.params.id]};
+		var update = {title: req.body.title, description: req.body.description};
+
+		Category.updateCategory(query, update, {}, function(err, category){
+			if(err){
+				res.send(err);
+			} else {
+				req.flash('success', 'Category Updated');
+				res.redirect('/manage/categories');
+			}
+		});
+	}
+});
+
+
+router.delete('/delte/:id', function(req, res){
+	var query = {_id: [req.params.id]};
+	Category.remove(query, function(err){
+		if(err){
+				res.send(err);
+			} else {
+				res.status(204).send();
+			}
+	});
+});
+
 module.exports = router;
