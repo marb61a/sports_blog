7f841f4e34ec057a29c472ee6b882fadc6287797
marb <marb61a@gmail.com>
2015-10-07 11:08:38 +0000

    upadted article view and model

--- routes/categories.js
+++ routes/categories.js
@@ -18,29 +18,29 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/add', function(req, res){
-    req.checkBody('title', 'Title is required').notEmpty();
-    
-    var errors = req.validationErrors();
-    
-    if(errors){
-        res.render('add_category', {
+	req.checkBody('title', 'Title is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('add_category', {
 			errors: errors,
 			title: "Add Category"
 		});
-    }else{
-        var category = new Category();
-        category.title - req.body.title;
-        category.description = req.body.description;
-        
-        Category.addCategory(category, function(err, category){
-            if(err){
+	} else {
+		var category = new Category();
+		category.title = req.body.title;
+		category.description = req.body.description;
+
+		Category.addCategory(category, function(err, category){
+			if(err){
 				res.send(err);
 			} else {
 				req.flash('success', 'Category Saved');
 				res.redirect('/manage/categories');
 			}
-        });
-    }
+		});
+	}
 });
 
 
